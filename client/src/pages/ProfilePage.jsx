2989f85856a700fee18d22e023f6770546ed0182
ShibhamKumarGupta <shivamlovesu2forever@gmail.com>
2025-06-29 20:09:08 +0530

    Update ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -36,7 +36,7 @@ const ProfilePage = () => {
         <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
           <input onChange={(e)=>setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png,.jpg,.jpeg' hidden/> 
           <img src={selectedImg?URL.createObjectURL(selectedImg): assets.avatar_icon} alt=""  className={`w-12 h-12 ${selectedImg && 'rounded-full'}`}/>
-          Upload Profile Image
+          Update Profile Image
         </label>
         <input onChange={(e)=>setName(e.target.value)} value={name}type="text" required placeholder='Your Name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500'/>
         <textarea onChange={(e)=>setBio(e.target.value)} value={bio}  placeholder='Write Profile bio' required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' rows={4}></textarea>
